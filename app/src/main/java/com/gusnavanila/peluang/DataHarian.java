9f637aa886f8090604c58644cf491a78e7b53981
gusna <gusna@GUSNAIKHSAN>
2018-11-29 09:08:50 +0700

    icon

---
+++ app/src/main/java/com/gusnavanila/peluang/DataHarian.java
@@ -0,0 +1,10 @@
+package com.gusnavanila.peluang;
+
+public class DataHarian {
+    public int pengeluaran;
+    public int pemasukan;
+    public String tanggal;
+
+    public DataHarian() {
+    }
+}
