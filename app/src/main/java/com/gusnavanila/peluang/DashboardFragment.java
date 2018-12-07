24262e254637f0d260aaa0ece0c57d96c247e4ed
gusna <gusna@GUSNAIKHSAN>
2018-12-07 09:20:18 +0700

    dashboard 70%

--- app/src/main/java/com/gusnavanila/peluang/DashboardFragment.java
+++ app/src/main/java/com/gusnavanila/peluang/DashboardFragment.java
@@ -1,6 +1,7 @@
 package com.gusnavanila.peluang;
 
 //import android.app.Fragment;
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -14,45 +15,50 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.gusnavanila.peluang.Database.DatabaseHelper;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class DashboardFragment extends Fragment {
     RecyclerView _recycleView, _recycleView2;
+    TextView _totalPemasukan,_totalPengeluaran;
     DataHarianAdapter _dataHarianAdapter;
     DataTransaksiHarianAdapter _dataTransaksiHarianAdapter;
     ArrayList<DataHarian> _dataHarian = new ArrayList<DataHarian>();
     ArrayList<DataTransaksiHarian> _dataTransaksiHarian = new ArrayList<DataTransaksiHarian>();
     Button _tambah;
+    DatabaseHelper _database;
+    String _tanggalHariIni;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
-
+        _database = new DatabaseHelper(getActivity());
         _tambah = rootView.findViewById(R.id.button);
+        _totalPemasukan = rootView.findViewById(R.id.totalPemasukan);
+        _totalPengeluaran = rootView.findViewById(R.id.totalPengeluaran);
+        Calendar cal = Calendar.getInstance();
+        _tanggalHariIni = String.valueOf(cal.get(Calendar.DAY_OF_MONTH)+"/"+(cal.get(Calendar.MONTH)+1)+"/"+cal.get(Calendar.YEAR));
 
-        _dataHarian.add(new DataHarian("10 Sep 2018",15000000,100000));
-        _dataHarian.add(new DataHarian("15 Sep 2018",3,100000));
-        _dataHarian.add(new DataHarian("17 Sep 2018",5,100000));
-        _dataHarian.add(new DataHarian("19 Sep 2018",300000,100000));
-        _dataHarian.add(new DataHarian("20 Sep 2018",300000,100000));
-        _dataHarian.add(new DataHarian("22 Sep 2018",300000,100000));
-        _dataHarian.add(new DataHarian("30 Sep 2018",300000,100000));
+        Cursor totalPengeluaran = _database.getTotal("Pengeluaran");
+        while(totalPengeluaran.moveToNext()){
+            _totalPengeluaran.setText("Rp. "+String.valueOf(totalPengeluaran.getInt(0)));
+        }
 
-        _recycleView = rootView.findViewById(R.id.recyclerView);
-        _dataHarianAdapter = new DataHarianAdapter(getActivity(),_dataHarian);
-        _recycleView.setAdapter(_dataHarianAdapter);
-        _recycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        Cursor totalPemasukan = _database.getTotal("Pemasukan");
+        while(totalPemasukan.moveToNext()){
+            _totalPemasukan.setText("Rp. "+String.valueOf(totalPemasukan.getInt(0)));
+        }
 
-        _dataTransaksiHarian.add(new DataTransaksiHarian("Makanan","Sarapan",15000));
-        _dataTransaksiHarian.add(new DataTransaksiHarian("Makanan","Makan Siang",15000));
-        _dataTransaksiHarian.add(new DataTransaksiHarian("Transportasi","Naik Lan",5000));
-        _dataTransaksiHarian.add(new DataTransaksiHarian("Makanan","Sarapan",15000));
-        _dataTransaksiHarian.add(new DataTransaksiHarian("Belanja","Jajan",10000));
-        _dataTransaksiHarian.add(new DataTransaksiHarian("Bayar Kas","Kas Org",5000));
-        _dataTransaksiHarian.add(new DataTransaksiHarian("Makanan","Sarapan",15000));
-        _dataTransaksiHarian.add(new DataTransaksiHarian("Makanan","Makan Siang",15000));
+        Cursor dataTransaksi = _database.getTransaksiHarian(_tanggalHariIni);
+        while (dataTransaksi.moveToNext()){
+            _dataTransaksiHarian.add(new DataTransaksiHarian(dataTransaksi.getString(1),dataTransaksi.getString(2),dataTransaksi.getInt(3)));
+        }
+//        _dataTransaksiHarian.add(new DataTransaksiHarian("Makanan","Makan Siang",15000));
 
         _recycleView2 = rootView.findViewById(R.id.recyclerView2);
         _dataTransaksiHarianAdapter = new DataTransaksiHarianAdapter(getActivity(),_dataTransaksiHarian);
@@ -64,6 +70,13 @@ public class DashboardFragment extends Fragment {
         Drawable horizontalDivider = ContextCompat.getDrawable(getActivity(), R.drawable.horizontal_divider);
         horizontalDecoration.setDrawable(horizontalDivider);
         _recycleView2.addItemDecoration(horizontalDecoration);
+
+        _dataHarian.add(new DataHarian("10 Sep 2018",15000000,100000));
+
+        _recycleView = rootView.findViewById(R.id.recyclerView);
+        _dataHarianAdapter = new DataHarianAdapter(getActivity(),_dataHarian);
+        _recycleView.setAdapter(_dataHarianAdapter);
+        _recycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         _tambah.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/gusnavanila/peluang/Database/DatabaseHelper.java
@@ -0,0 +1,145 @@
+package com.gusnavanila.peluang.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "peluang_db";
+    private static final int DATABASE_VERSION = 1;
+
+//    kolom tabel transaksi
+    public static final String TABEL_TRANSAKSI = "transaksi";
+    public static final String ID_TRANSAKSI = "id_transaksi";
+    public static final String ID_KATEGORI_T = "id_kategori";
+    public static final String NOMINAL = "nominal";
+    public static final String DESKRIPSI = "deskripsi";
+    public static final String TANGGAL = "tanggal";
+//    kolom tabel tipe
+    public static final String TABEL_TIPE = "tipe_kategori";
+    public static final String ID_KATEGORI = "id_kategori";
+    public static final String TIPE = "tipe";
+    public static final String KATEGORI = "kategori";
+
+    public static final String CREATE_TRANSAKSI_TABLE = "CREATE TABLE "
+            + TABEL_TRANSAKSI + "("
+            + ID_TRANSAKSI + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + ID_KATEGORI_T + " INT, "
+            + NOMINAL + " INT, "
+            + DESKRIPSI + " TEXT, "
+            + TANGGAL + " TEXT, "
+            + "FOREIGN KEY(" + ID_KATEGORI_T + ") REFERENCES "
+            + TABEL_TIPE + "("+ ID_KATEGORI +") " + ")";
+
+    public static final String CREATE_TIPE_TABLE = "CREATE TABLE "
+            + TABEL_TIPE + "("
+            + ID_KATEGORI + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + TIPE + " TEXT, "
+            + KATEGORI + " TEXT )";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TRANSAKSI_TABLE);
+        db.execSQL(CREATE_TIPE_TABLE);
+        db.execSQL("INSERT INTO tipe_kategori"+
+                "(id_kategori,tipe,kategori)"+
+                " VALUES "+
+                "(0,'Pemasukan','Gaji')");
+
+        db.execSQL("INSERT INTO tipe_kategori"+
+                "(id_kategori,tipe,kategori)"+
+                " VALUES "+
+                "(1,'Pemasukan','Hasil Usaha')");
+
+        db.execSQL("INSERT INTO tipe_kategori"+
+                "(id_kategori,tipe,kategori)"+
+                " VALUES "+
+                "(2,'Pengeluaran','Makanan')");
+
+        db.execSQL("INSERT INTO tipe_kategori"+
+                "(id_kategori,tipe,kategori)"+
+                " VALUES "+
+                "(3,'Pengeluaran','Minuman')");
+
+        db.execSQL("INSERT INTO tipe_kategori"+
+                "(id_kategori,tipe,kategori)"+
+                " VALUES "+
+                "(4,'Pengeluaran','Jajanan')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String drop = "DROP IF TABLE EXISTS";
+        db.execSQL(drop+ TABEL_TRANSAKSI);
+        db.execSQL(drop+TABEL_TIPE);
+        onCreate(db);
+    }
+
+    public boolean addData(String tipe, int kategori,int jumlah, String deskripsi, String tanggal){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        long result;
+
+        cv.put(ID_KATEGORI_T,kategori);
+        cv.put(NOMINAL,jumlah);
+        cv.put(DESKRIPSI,deskripsi);
+        cv.put(TANGGAL,tanggal);
+
+        result = db.insert(TABEL_TRANSAKSI,null,cv);
+
+
+        if (result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Cursor getIdTipe(String tipe, String kategori){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABEL_TIPE +" WHERE tipe = '"+tipe+"' AND kategori = '"+kategori+"'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public Cursor getKategori(String tipe){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT kategori FROM " + TABEL_TIPE +" WHERE tipe = '"+tipe+"'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public Cursor getTransaksiHarian(String tanggal){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT tipe,kategori,deskripsi,nominal FROM transaksi JOIN tipe_kategori"
+                +" ON transaksi.id_kategori = tipe_kategori.id_kategori"
+                +" WHERE tanggal = '"+tanggal+"'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public Cursor getTotal(String tipe){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT sum(nominal) FROM transaksi JOIN tipe_kategori"
+                +" ON transaksi.id_kategori = tipe_kategori.id_kategori"
+                +" WHERE tipe_kategori.tipe = '"+tipe+"'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+}
